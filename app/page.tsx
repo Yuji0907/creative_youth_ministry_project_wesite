d3c46a5ad87c36e4bc7aa8ee6304b6b2c20c1411
Benzaama Abdallah younes <84568210+Yuji0907@users.noreply.github.com>
2026-09-12 00:40:42 +0100

    building the hero page for  desktop

--- app/page.tsx
+++ app/page.tsx
@@ -1,69 +1,99 @@
 import Image from "next/image";
-import styles from "./page.module.css";
-
+import  "./page.css";
+import androidImage from "@/public/assets/android_blured.png";
+import kotlinImage from "@/public/assets/kotli_blured.png";
+import creativeLogo from "@/public/assets/icons/creaative_icon.svg";
+import gridImage from "@/public/assets/grid png white background transparent_21045126 copy 2.png";
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
+    <div className="grid">
+      <Image
+              className="creaative-icon"
+              src={creativeLogo}
+              alt="sads"
         />
-        <div className={styles.intro}>
-          <h1>
-            To get started, edit the{" "}
-            <code className={styles.code}>page.tsx</code> file.
-          </h1>
+      <Image
+              className="android-image"
+              src={androidImage}
+              alt="sads"
+        />
+        <Image
+              className="grid-image"
+              src={gridImage}
+              alt="sads"
+        />
+        <Image
+              className="grid-image-02"
+              src={gridImage}
+              alt="sads"
+        />
+        <Image
+              className="kotlin-image"
+              src={kotlinImage}
+              alt="sads"
+        />
+      <main className="main">
+
+        <div className="intro slideUpFade">
+            <h1>
+              <span>
+                <div className="rect-01" />
+                <div className="rect-02" />
+                <div className="rect-03" />
+                <div className="rect-04" />
+                برمج سوق
+
+              </span>
+              <span>  و ارتقي</span>
+            </h1>
+            <h2>
+              <span>إلى العالم الرقمي </span>
+            </h2>
+        </div>
+        <div className="slogen ">
+          <p> 
+            <span>حوّل فكرتك إلى تطبيق حقيقي</span>
+            <span>kotlin تعلم </span>
+          </p>
           <p>
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learning
-            </a>{" "}
-            center.
+            <span>استخدم الذكاء الاصطناعي، ابنِ منتجك، وتعلّم كيف توصله إلى جمهورك.</span>
           </p>
         </div>
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={14}
-            />
-            Deploy Now
-          </a>
-          <a
-            className={styles.secondary}
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
+        <button className="cta" type="button">
+          سجل الآن
+        </button>
+
       </main>
+              <div className="flex-group pop">
+          <Image
+              className="saas-icons"
+              src="/assets/icons/ai-90.png"
+              alt="sads"
+              width={30}
+              height={30}
+            />
+          <Image
+              className="saas-icons"
+              src="/assets/icons/android-100.png"
+              alt="sads"
+              width={30}
+              height={30}
+            />
+          <Image
+              className="saas-icons"
+              src="/assets/icons/kotlin-96.png"
+              alt="sads"
+              width={30}
+              height={30}
+            />
+          <Image
+              className="saas-icons"
+              src="/assets/icons/marketing-96.png"
+              alt="sads"
+              width={30}
+              height={30}
+            />
+        </div>
     </div>
   );
 }
